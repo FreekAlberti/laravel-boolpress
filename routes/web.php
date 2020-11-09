bb2f9e53596b1c5a4b1b44eac230d9b51c152190
FreekAlberti <federicoalberi96@gmail.com>
2020-11-09 16:26:39 +0100

    index admin

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,8 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix("admin")->namespace("Admin")->middleware("auth")->group(function() {
+    Route::resource("posts", "PostController");
+});
